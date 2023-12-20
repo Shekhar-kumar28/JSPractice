0b4ab89dae83c30b230e906fdcc907d637c9a64b
Shekhar Kumar <sk164949@gmail.com>
2023-12-20 13:06:25 +0530

    1st parts

---
+++ variable.js
@@ -0,0 +1,117 @@
+// var a=10;
+// var b = 20;
+// name= "Shekhar";
+// console.log(b,a, name);
+
+
+// Data Type In js
+// Primitive Types:
+
+// var a = 25;
+// age = 43;
+// console.log("Primitive data types are :",typeof(a));
+// console.log("Primitive data types are :",typeof(age));
+//   name = "Shekhar";
+//    console.log("Primitive data type are:",typeof(name));
+
+// dispalcement = -34
+// console.log(typeof(dispalcement)) 
+
+// var b= 34;
+// console.log(typeof(a+b));
+
+
+// operation in js
+
+// a = 20;
+// b = 10;
+// console.log(a + b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// NaN in JS
+
+// The NaN global property is a value representing Not-A-Number.
+
+// console.log(0/0);
+// console.log(NaN-1);
+// console.log(NaN+1);
+// console.log(NaN*1);
+// console.log(typeof(0/0));
+
+
+// operator Precedence:- This is the general order of solving an expression.
+//  () , ** , * , / , % 
+
+// console.log((2+2)*3)
+// console.log(3/1+2**2)
+// console.log(4+1*6/2)
+
+
+// let keyword:
+
+// let age = 23;
+// console.log(age = age+1);
+
+// let cgpa = 8.9;
+// console.log(cgpa += .5);
+
+// let side = 4;
+// console.log(side*side) 
+
+//constant 
+
+// const year = 2025
+// console.log(year)
+// year = 2023// error
+
+// var Keyword
+// old syntax of writing variable.
+
+// var age = 23;
+// var cgpa = 8.9;
+
+// console.log(age)
+// console.log(cgpa)
+
+// Q: What is the value of age after this code runs?
+
+// let age = 23;
+// age=age+2;
+// console.log(age)
+
+// Q2: What is the value of avg after this code runs?
+
+// let hindi = 80;
+// let eng = 90;
+// let math = 100;
+// let avg = (hindi +eng + math)/3;
+// console.log(avg);
+
+// assingment operator
+
+// let age = 23;
+// // age = age+1;
+// age = age-1;
+// console.log(age);
+
+//Q: What is the value of each variable in each lin of code?
+
+// let num  = 5;
+// let newNum = num++;
+// newNum = ++ num;
+// console.log(newNum)
+
+
+// Identifier Rules..
+
+// All javaScript variable must be identified with unique names(identifiers).
+//     ||| Names must contain letter, digits, underscores,and dollar signs. (no space).
+//     ||| Names must begin with a letter.
+//     ||| Names must also begin with $and_.
+//     ||| Names are case sensitive (y and Y are different variables)
+//     ||| Reserved words (likes javascript keywords keywords) CANNOT be used as names.
