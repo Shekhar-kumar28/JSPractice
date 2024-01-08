f9929f94131a52fce0d84373cde626186427b611
Shekhar Kumar <sk164949@gmail.com>
2024-01-08 21:07:49 +0530

    changes

--- Express/index.js
+++ Express/index.js
@@ -1,35 +1,53 @@
+// const express = require("express");
+
+// const app = express();
+// // console.dir(app)
+
+// let port = 3000;
+
+// app.listen(port,()=>{
+//     console.log(`App is listening on port:${port}`);
+// })
+
+// // app.get("/",(req,res) => {
+// //     res.send("<h1>Hello World!</h1>")
+// // })
+// // app.get("/Apple",(req,res) => {
+// //     res.send("Hey I am using apple path");
+// // })
+// // app.get("/Oraa",(req,res) => {
+// //     res.send("Het I am using Orange Path");
+// // })
+
+// app.get("/search",(req,res) => {
+//     console.log(req.query);
+//     res.send("np result");
+// })
+
+// // app.use((req,res) => {
+// //     console.log("request recieved")
+// //     res.send("Hello World");
+// // })
+
+
+
 const express = require("express");
-
 const app = express();
-// console.dir(app)
+const path = require("path");
 
-let port = 3000;
+const port = 8080;
 
-app.listen(port,()=>{
-    console.log(`App is listening on port:${port}`);
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req,res) => {
+    res.render("home.ejs")
 })
 
-// app.get("/",(req,res) => {
-//     res.send("<h1>Hello World!</h1>")
-// })
-// app.get("/Apple",(req,res) => {
-//     res.send("Hey I am using apple path");
-// })
-// app.get("/Oraa",(req,res) => {
-//     res.send("Het I am using Orange Path");
-// })
-
-app.get("/search",(req,res) => {
-    console.log(req.query);
-    res.send("np result");
+app.get("/home",(req,res) => {
+    res.send("Hello")
 })
 
-
-
-
-
-
-// app.use((req,res) => {
-//     console.log("request recieved")
-//     res.send("Hello World");
-// })
+app.listen(port,() => {
+    console.log(`Server started at ${port}`)
+});
