fcd4101f2c430f5c1e7fb04174fe7d3680e0f73c
Shekhar Kumar <sk164949@gmail.com>
2024-01-19 21:48:34 +0530

    changes

--- SQL/index.js
+++ SQL/index.js
@@ -4,12 +4,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const { v4: uuidv4 } = require("uuid");
+
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
+let getUser = () => {
+    return [
+      faker.datatype.uuid(),
+      faker.internet.userName(),
+      faker.internet.email(),
+      faker.internet.password(),
+    ];
+  };
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -72,8 +82,8 @@ app.get("/", (req, res) => {
     } catch (err) {
         console.log(err);
         res.render("some erroe in db");
-    }
-})
+    };
+});
 
 // show Route
 app.get("/user",(req,res) =>{
@@ -130,6 +140,74 @@ app.patch("/user/:id",(req,res) => {
         res.send("some error in DB");
     }
 })
+
+app.get("/user/new", (req, res) => {
+    res.render("new.ejs");
+  });
+  
+  app.post("/user/new", (req, res) => {
+    let { username, email, password } = req.body;
+    let id = uuidv4();
+    //Query to Insert New User
+    let q = `INSERT INTO user (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+  
+    try {
+      connection.query(q, (err, result) => {
+        if (err) throw err;
+        console.log("added new user");
+        res.redirect("/user");
+      });
+    } catch (err) {
+      res.send("some error occurred");
+    }
+  });
+  
+  app.get("/user/:id/delete", (req, res) => {
+    let { id } = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+  
+    try {
+      connection.query(q, (err, result) => {
+        if (err) throw err;
+        let user = result[0];
+        res.render("delete.ejs", { user });
+      });
+    } catch (err) {
+      res.send("some error with DB");
+    }
+  });
+  
+  app.delete("/user/:id/", (req, res) => {
+    let { id } = req.params;
+    let { password } = req.body;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+  
+    try {
+      connection.query(q, (err, result) => {
+        if (err) throw err;
+        let user = result[0];
+  
+        if (user.password != password) {
+          res.send("WRONG Password entered!");
+        } else {
+          let q2 = `DELETE FROM user WHERE id='${id}'`; //Query to Delete
+          connection.query(q2, (err, result) => {
+            if (err) throw err;
+            else {
+              console.log(result);
+              console.log("deleted!");
+              res.redirect("/user");
+            }
+          });
+        }
+      });
+    } catch (err) {
+      res.send("some error with DB");
+    }
+  });
+
+
+
 
 app.listen("8080", () => {
     console.log("Server is running on port 8080");
